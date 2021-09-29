64c1f30eac9cd5463c9c6df2e70592d4b70b828d
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-09-29 17:30:03 -0300

    init next.js project for the website 'Circuito BH'

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from 'next/document';
+
+const description = '\
+A Circuito BH surgiu com a proposta de assessorar músicos e bandas \
+de Belo Horizonte, promovendo shows e contribuindo para a inserção \
+desses artistas no cenário musical da capital mineira.\
+'
+const Document = () => {
+  return (
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="icon" type="image/png" href="/icon.png" />
+        <meta name="description" content={description} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document;
+
