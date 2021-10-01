ae2979da7ef20fb0062b212cdd2191750d43e85e
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-10-01 19:37:32 -0300

    fix linting errors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,21 +4,21 @@ const description = '\
 A Circuito BH surgiu com a proposta de assessorar músicos e bandas \
 de Belo Horizonte, promovendo shows e contribuindo para a inserção \
 desses artistas no cenário musical da capital mineira.\
-'
+';
 const Document = () => {
   return (
-    <Html lang="pt-BR">
+    <Html lang='pt-BR'>
       <Head>
-        <link rel="icon" type="image/png" href="/icon.png" />
-        <meta name="description" content={description} />
+        <link rel='icon' type='image/png' href='/icon.png' />
+        <meta name='description' content={description} />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
 
 export default Document;
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
       </Head>
 
     </>
-  )
-}
+  );
+};
 
 export default Home;
