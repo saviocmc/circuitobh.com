0d09f7425b8a99f61c69b3f47631ba882f12da65
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-10-29 02:19:28 -0300

    header: logo: set fixed height and width

--- components/header.tsx
+++ components/header.tsx
@@ -3,9 +3,7 @@ import css from './header.module.css';
 const Header = () => {
   return (
     <header className={css.Header}>
-
-      <img src='/logo.png' alt='Logo da Circuito BH' />
-
+      <img src='/logo.png' alt='Logo da Circuito BH' className={css.Logo} />
     </header>
   );
 };
