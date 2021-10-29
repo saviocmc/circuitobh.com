5d9a4bb66a36c2dc385e422930072eae26528921
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-10-29 00:30:58 -0300

    Revert "header: use the image component from 'next/image' for the png logo"
    This reverts commit 8170c346875795622c802f6e86371c7e086058e7.

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image';
 import css from './header.module.css';
 
 const Header = () => {
   return (
     <header className={css.Header}>
-      <Image src='/logo.png' alt='Logo da Circuito BH' width={570} height={200} />
+
+      <img src='/logo.png' alt='Logo da Circuito BH' />
+
     </header>
   );
 };
