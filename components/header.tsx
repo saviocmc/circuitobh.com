8170c346875795622c802f6e86371c7e086058e7
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2021-10-29 00:23:32 -0300

    header: use the image component from 'next/image' for the png logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,10 @@
+import Image from 'next/image';
 import css from './header.module.css';
 
 const Header = () => {
   return (
     <header className={css.Header}>
-
-      <img src='/logo.png' alt='Logo da Circuito BH' />
-
+      <Image src='/logo.png' alt='Logo da Circuito BH' width={570} height={200} />
     </header>
   );
 };
