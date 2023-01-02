416b4affbaff041886f60a2b7e56c34320b525d5
Sávio Carlos Martins Costa <saviocmc@protonmail.com>
2023-01-01 21:28:06 -0300

    fix: update the YouTube channel link and fix naming of icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,8 @@ import css from './footer.module.css';
 import FacebookIcon from './icons/facebook.svg';
 import InstagramIcon from './icons/instagram.svg';
 import MailIcon from './icons/mail.svg';
-import WhatsappIcon from './icons/whatsapp.svg';
-import YoutubeIcon from './icons/youtube.svg';
+import WhatsAppIcon from './icons/whatsapp.svg';
+import YouTubeIcon from './icons/youtube.svg';
 
 const Footer = () => {
   return (
@@ -16,14 +16,14 @@ const Footer = () => {
           <FacebookIcon className={css.Icon} />
         </a>
 
-        <a href='https://instagram.com/circuito_bh/'
+        <a href='https://instagram.com/circuito_bh'
           rel='noreferrer noopener' target='_blank' title='Perfil no Instagram'>
           <InstagramIcon className={css.Icon} />
         </a>
 
-        <a href='https://www.youtube.com/channel/UCP2AO8BXKQacuiFFn_Gq_kA'
+        <a href='https://www.youtube.com/@circuitobh2586'
           rel='noreferrer noopener' target='_blank' title='Canal no Youtube'>
-          <YoutubeIcon className={css.Icon} />
+          <YouTubeIcon className={css.Icon} />
         </a>
 
       </div>
@@ -34,7 +34,7 @@ const Footer = () => {
         <MailIcon className={css.Icon} />
 
         <a href='https://api.whatsapp.com/send?phone=+5531987016193'>(31) 98701-6193</a>
-        <WhatsappIcon className={css.Icon} />
+        <WhatsAppIcon className={css.Icon} />
 
       </div>
 
